f14d67f817baf1ef9312e431c9d27b1a162d734c
vishxlkr <vishxlkr@gmail.com>
2025-07-20 19:16:18 +0530

    update

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import assets from "../assets/assets";
+import { AuthContext } from "../../context/AuthContext";
 
 const LoginPage = () => {
    const [currState, setCurrState] = useState("Sign up");
@@ -9,6 +10,8 @@ const LoginPage = () => {
    const [bio, setBio] = useState("");
    const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+   const { login } = useContext(AuthContext);
+
    const onSubmitHandler = (event) => {
       event.preventDefault();
 
@@ -16,6 +19,13 @@ const LoginPage = () => {
          setIsDataSubmitted(true);
          return;
       }
+
+      login(currState === "Signup" ? "signup" : "login", {
+         fullName,
+         email,
+         password,
+         bio,
+      });
    };
    return (
       <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
