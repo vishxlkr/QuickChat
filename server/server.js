dff3fe1689cbbf039cb2e7e3836b063f622652b4
vishxlkr <vishxlkr@gmail.com>
2025-07-20 08:40:58 +0530

    update

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // create expres ap and http server
 const app = express();
@@ -12,12 +13,18 @@ const server = http.createServer(app);
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
+// Routes setup
 app.use("/api/status", (req, res) => res.send("Server is Live."));
+app.use("/api/auth", userRouter);
 
 // connect to mongoDb
-await connectDB();
+const startServer = async () => {
+   await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-   console.log("Server is running on PORT " + PORT);
-});
+   const PORT = process.env.PORT || 5000;
+   server.listen(PORT, () => {
+      console.log("Server is running on PORT " + PORT);
+   });
+};
+
+startServer();
