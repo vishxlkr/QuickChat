59ec3d99661276cfccab6e311701b04efdd7c41f
vishxlkr <vishxlkr@gmail.com>
2025-07-30 05:45:06 +0530

    update

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -5,7 +5,6 @@ import { io } from "socket.io-client";
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.baseURL = backendUrl;
-axios.defaults.withCredentials = true;
 
 export const AuthContext = createContext();
 
