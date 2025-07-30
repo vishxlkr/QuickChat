0979778bd66720404913a29ff5b9a6fc32d7d693
vishxlkr <vishxlkr@gmail.com>
2025-07-31 03:26:25 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+   plugins: [react(), tailwindcss()],
+   preview: {
+      port: 4173, // or any you prefer
+      host: true,
+      allowedHosts: ["quickchat-1-dr5u.onrender.com"],
+   },
 });
